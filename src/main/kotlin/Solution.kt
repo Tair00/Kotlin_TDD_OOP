c97810e5d2ee1ac3f854118b58231b947f029b56
Tair00 <suleimanov.t.i.20@gmail.com>
2024-03-21 18:10:44 +0300

    find-the-pivot-integer

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,42 +1,24 @@
-fun longestPalindrome(s: String): String {
-    if (s.length < 2) return s
 
-    var start = 0
-    var maxLength = 1
+    fun pivotInteger(n: Int): Int {
+        var sum = 0
+        var leftSum = 0
 
-    for (i in s.indices) {
-        var low = i - maxLength - 1
-        var high = i
-
-        while (low >= 0 && high < s.length && s[low] == s[high]) {
-            if (high - low + 1 > maxLength) {
-                start = low
-                maxLength = high - low + 1
-            }
-            low--
-            high++
+        for (i in 1..n) {
+            sum += i
         }
 
-        low = i - maxLength
-        high = i
-
-        while (low >= 0 && high < s.length && s[low] == s[high]) {
-            if (high - low + 1 > maxLength) {
-                start = low
-                maxLength = high - low + 1
+        for (i in 1..n) {
+            sum -= i
+            if (leftSum == sum) {
+                return i
             }
-            low--
-            high++
+            leftSum += i
         }
+
+
+        return -1
     }
 
-    return s.substring(start, start + maxLength)
-}
 
 fun main() {
-    val s1 = "babad"
-    println(longestPalindrome(s1))  // Вывод: "bab" или "aba"
-
-    val s2 = "cbbd"
-    println(longestPalindrome(s2))  // Вывод: "bb"
 }
--- src/test/kotlin/SolutionKtTest.kt
+++ src/test/kotlin/SolutionKtTest.kt
@@ -5,13 +5,25 @@ class SolutionKtTest {
 
     @Test
     fun testLongestPalindrome() {
-        assertEquals("bab", longestPalindrome("babad"))
-        assertEquals("bb", longestPalindrome("cbbd"))
-        assertEquals("a", longestPalindrome("a"))
-        assertEquals("aaaaa", longestPalindrome("aaaaa"))
-        assertEquals("abcba", longestPalindrome("abcba"))
-        assertEquals("aabbccbbaa", longestPalindrome("aabbccbbaa"))
-        assertEquals("abccba", longestPalindrome("abccbadef"))
-        assertEquals("aaaa", longestPalindrome("aaaaefgh"))
+
+        @Test
+        fun testPivotWithValidInput() {
+            assertEquals(6, pivotInteger(8))
+        }
+
+        @Test
+        fun testPivotWithBoundaryInput() {
+            assertEquals(1, pivotInteger(1))
+        }
+
+        @Test
+        fun testPivotWithNoPivot() {
+            assertEquals(-1, pivotInteger(4))
+        }
+
+        @Test
+        fun testPivotWithLargeInput() {
+            assertEquals(500, pivotInteger(1000))
+        }
     }
 }
