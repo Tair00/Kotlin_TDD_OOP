9f352df0fec7f2912522d516374e884a177def40
Tair00 <suleimanov.t.i.20@gmail.com>
2024-03-23 16:22:19 +0300

    longest-common-prefix

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,31 +1,21 @@
-class Solution {
-    fun romanToInt(s: String): Int {
-        val romanMap = mapOf(
-            'I' to 1,
-            'V' to 5,
-            'X' to 10,
-            'L' to 50,
-            'C' to 100,
-            'D' to 500,
-            'M' to 1000
-        )
 
-        var result = 0
-        var prevValue = 0
+    class Solution {
+        fun longestCommonPrefix(strs: Array<String>): String {
+            if (strs.isEmpty()) return ""
 
-        for (c in s.reversed()) {
-            val value = romanMap[c]!!
-            if (value < prevValue) {
-                result -= value
-            } else {
-                result += value
+            for (i in strs[0].indices) {
+                val char = strs[0][i]
+                for (j in 1 until strs.size) {
+                    if (i == strs[j].length || strs[j][i] != char) {
+                        return strs[0].substring(0, i)
+                    }
+                }
             }
-            prevValue = value
+
+            return strs[0]
         }
-
-        return result
     }
-}
 
-fun main() {
-}
+    fun main() {
+
+    }
--- src/test/kotlin/SolutionKtTest.kt
+++ src/test/kotlin/SolutionKtTest.kt
@@ -5,28 +5,25 @@ class SolutionKtTest {
 
     private val solution = Solution()
 
+
+
     @Test
-    fun testRomanToInt() {
-        assertEquals(3, solution.romanToInt("III"))
-        assertEquals(58, solution.romanToInt("LVIII"))
-        assertEquals(1994, solution.romanToInt("MCMXCIV"))
+    fun testEmptyInput() {
+        assertEquals("", solution.longestCommonPrefix(emptyArray()))
     }
 
     @Test
-    fun testEdgeCases() {
-        assertEquals(1, solution.romanToInt("I"))
-        assertEquals(3999, solution.romanToInt("MMMCMXCIX"))
-        assertEquals(1000, solution.romanToInt("M"))
-        assertEquals(10, solution.romanToInt("X"))
+    fun testSingleString() {
+        assertEquals("abc", solution.longestCommonPrefix(arrayOf("abc")))
     }
 
     @Test
-    fun testSubtractiveNotation() {
-        assertEquals(4, solution.romanToInt("IV"))
-        assertEquals(9, solution.romanToInt("IX"))
-        assertEquals(40, solution.romanToInt("XL"))
-        assertEquals(90, solution.romanToInt("XC"))
-        assertEquals(400, solution.romanToInt("CD"))
-        assertEquals(900, solution.romanToInt("CM"))
+    fun testAllEqualStrings() {
+        assertEquals("abc", solution.longestCommonPrefix(arrayOf("abc", "abc", "abc")))
+    }
+
+    @Test
+    fun testNoCommonPrefix() {
+        assertEquals("", solution.longestCommonPrefix(arrayOf("xyz", "123", "!@#")))
     }
 }
