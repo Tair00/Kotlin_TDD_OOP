d3b5c49def443649fbaa40eff704742ef8e6ddf8
Tair00 <suleimanov.t.i.20@gmail.com>
2024-03-23 16:12:04 +0300

    roman-to-integer

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,24 +1,31 @@
+class Solution {
+    fun romanToInt(s: String): Int {
+        val romanMap = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
 
-    fun pivotInteger(n: Int): Int {
-        var sum = 0
-        var leftSum = 0
+        var result = 0
+        var prevValue = 0
 
-        for (i in 1..n) {
-            sum += i
-        }
-
-        for (i in 1..n) {
-            sum -= i
-            if (leftSum == sum) {
-                return i
+        for (c in s.reversed()) {
+            val value = romanMap[c]!!
+            if (value < prevValue) {
+                result -= value
+            } else {
+                result += value
             }
-            leftSum += i
+            prevValue = value
         }
 
-
-        return -1
+        return result
     }
-
+}
 
 fun main() {
 }
--- src/test/kotlin/SolutionKtTest.kt
+++ src/test/kotlin/SolutionKtTest.kt
@@ -3,27 +3,30 @@ import kotlin.test.Test
 
 class SolutionKtTest {
 
+    private val solution = Solution()
+
     @Test
-    fun testLongestPalindrome() {
+    fun testRomanToInt() {
+        assertEquals(3, solution.romanToInt("III"))
+        assertEquals(58, solution.romanToInt("LVIII"))
+        assertEquals(1994, solution.romanToInt("MCMXCIV"))
+    }
 
-        @Test
-        fun testPivotWithValidInput() {
-            assertEquals(6, pivotInteger(8))
-        }
+    @Test
+    fun testEdgeCases() {
+        assertEquals(1, solution.romanToInt("I"))
+        assertEquals(3999, solution.romanToInt("MMMCMXCIX"))
+        assertEquals(1000, solution.romanToInt("M"))
+        assertEquals(10, solution.romanToInt("X"))
+    }
 
-        @Test
-        fun testPivotWithBoundaryInput() {
-            assertEquals(1, pivotInteger(1))
-        }
-
-        @Test
-        fun testPivotWithNoPivot() {
-            assertEquals(-1, pivotInteger(4))
-        }
-
-        @Test
-        fun testPivotWithLargeInput() {
-            assertEquals(500, pivotInteger(1000))
-        }
+    @Test
+    fun testSubtractiveNotation() {
+        assertEquals(4, solution.romanToInt("IV"))
+        assertEquals(9, solution.romanToInt("IX"))
+        assertEquals(40, solution.romanToInt("XL"))
+        assertEquals(90, solution.romanToInt("XC"))
+        assertEquals(400, solution.romanToInt("CD"))
+        assertEquals(900, solution.romanToInt("CM"))
     }
 }
