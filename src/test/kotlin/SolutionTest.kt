6fd3575c6131dc5a93f919fa29209053f16a605d
Tair00 <suleimanov.t.i.20@gmail.com>
2024-04-04 15:36:23 +0300

    palindrome-number

--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -1,27 +1,25 @@
 import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 
+
+
 class SolutionTest {
     private val solution = Solution()
 
     @Test
-    fun testLengthOfLongestSubstring() {
+    fun testReverse() {
+        // Положительные числа
+        assertEquals(321, solution.reverse(123))
+        assertEquals(54321, solution.reverse(12345))
+        assertEquals(21, solution.reverse(120))
 
+        // Отрицательные числа
+        assertEquals(-321, solution.reverse(-123))
+        assertEquals(-54321, solution.reverse(-12345))
 
-            @Test
-            fun testIsPalindrome() {
-
-                assertEquals(true, solution.isPalindrome(0))
-                assertEquals(true, solution.isPalindrome(121))
-                assertEquals(true, solution.isPalindrome(12321))
-                assertEquals(true, solution.isPalindrome(1234321))
-
-
-                assertEquals(false, solution.isPalindrome(-121))
-                assertEquals(false, solution.isPalindrome(10))
-                assertEquals(false, solution.isPalindrome(123456))
-                assertEquals(false, solution.isPalindrome(12345678))
-            }
-        }
-
+        // Особые случаи
+        assertEquals(0, solution.reverse(0))
+        assertEquals(0, solution.reverse(1534236469)) // Превышение максимального значения
+        assertEquals(0, solution.reverse(-2147483648)) // Превышение минимального значения
     }
+}
