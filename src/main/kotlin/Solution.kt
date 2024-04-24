9a95674ca5a52d4ef17dcd510f5f32fed118e92c
Tair00 <suleimanov.t.i.20@gmail.com>
2024-04-24 21:23:42 +0300

    remove-nth-node-from-end-of-list

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,34 +1,31 @@
 /**
  * Definition for singly-linked list.
  */
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
-
 class Solution {
-    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
 
-        val dummyHead = ListNode(0)
-        var current: ListNode? = dummyHead
-        var p1 = list1
-        var p2 = list2
+        val dummy = ListNode(0)
+        dummy.next = head
+        var first: ListNode? = dummy
+        var second: ListNode? = dummy
 
-        while (p1 != null && p2 != null) {
 
-            if (p1.`val` < p2.`val`) {
-                current?.next = p1
-                p1 = p1.next
-            } else {
-                current?.next = p2
-                p2 = p2.next
-            }
-            current = current?.next
+        for (i in 1..n + 1) {
+            first = first?.next
         }
 
 
-        current?.next = p1 ?: p2
+        while (first != null) {
+            first = first.next
+            second = second?.next
+        }
 
 
-        return dummyHead.next
+        second?.next = second?.next?.next
+
+        return dummy.next
     }
+}
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
 }
--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -3,48 +3,41 @@ import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 
 
-class SolutionTest {
-
+internal class SolutionTest {
     @Test
-    fun testMergeTwoLists() {
-
-        val list1 = createLinkedList(intArrayOf(1, 2, 4))
-        val list2 = createLinkedList(intArrayOf(1, 3, 4))
-
+    fun testRemoveNthFromEnd() {
         val solution = Solution()
-        val mergedList = solution.mergeTwoLists(list1, list2)
 
+        // Example 1
+        var head = ListNode(1)
+        head.next = ListNode(2)
+        head.next?.next = ListNode(3)
+        head.next?.next?.next = ListNode(4)
+        head.next?.next?.next?.next = ListNode(5)
+        var newHead = solution.removeNthFromEnd(head, 2)
+        var expected = intArrayOf(1, 2, 3, 5)
+        assertArrayEquals(expected, convertToArray(newHead))
 
-        val expectedValues = intArrayOf(1, 1, 2, 3, 4, 4)
-        val expectedList = createLinkedList(expectedValues)
+        // Example 2
+        head = ListNode(1)
+        newHead = solution.removeNthFromEnd(head, 1)
+        assertNull(newHead)
 
-
-        Assert.assertTrue(areListsEqual(expectedList, mergedList))
+        // Example 3
+        head = ListNode(1)
+        head.next = ListNode(2)
+        newHead = solution.removeNthFromEnd(head, 1)
+        expected = intArrayOf(1)
+        assertArrayEquals(expected, convertToArray(newHead))
     }
 
-    private fun createLinkedList(values: IntArray): ListNode? {
-        if (values.isEmpty()) return null
-        val head = ListNode(values[0])
+    private fun convertToArray(head: ListNode?): IntArray {
+        val result = mutableListOf<Int>()
         var current: ListNode? = head
-        for (i in 1 until values.size) {
-            current?.next = ListNode(values[i])
-            current = current?.next
+        while (current != null) {
+            result.add(current.`val`)
+            current = current.next
         }
-        return head
-    }
-
-    private fun areListsEqual(list1: ListNode?, list2: ListNode?): Boolean {
-        var p1: ListNode? = list1
-        var p2: ListNode? = list2
-
-        while (p1 != null && p2 != null) {
-            if (p1.`val` != p2.`val`) {
-                return false
-            }
-            p1 = p1.next
-            p2 = p2.next
-        }
-
-        return p1 == null && p2 == null
+        return result.toIntArray()
     }
 }
