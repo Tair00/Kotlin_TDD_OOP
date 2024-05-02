c8043c6785a9621bd90f62a13c63295dbe1fdc47
Tair00 <suleimanov.t.i.20@gmail.com>
2024-05-02 15:58:08 +0300

    integer-to-roman

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,31 +1,19 @@
-/**
- * Definition for singly-linked list.
- */
+
 class Solution {
-    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    fun intToRoman(num: Int): String {
+        val decimalValues = intArrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+        val romanSymbols = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
 
-        val dummy = ListNode(0)
-        dummy.next = head
-        var first: ListNode? = dummy
-        var second: ListNode? = dummy
+        var result = ""
+        var remaining = num
 
-
-        for (i in 1..n + 1) {
-            first = first?.next
+        for (i in decimalValues.indices) {
+            while (remaining >= decimalValues[i]) {
+                result += romanSymbols[i]
+                remaining -= decimalValues[i]
+            }
         }
 
-
-        while (first != null) {
-            first = first.next
-            second = second?.next
-        }
-
-
-        second?.next = second?.next?.next
-
-        return dummy.next
+        return result
     }
-}
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
 }
--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -3,41 +3,23 @@ import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
 
 
-internal class SolutionTest {
+
+
+
+
+class SolutionTest {
     @Test
-    fun testRemoveNthFromEnd() {
+    fun testIntToRoman() {
         val solution = Solution()
 
-        // Example 1
-        var head = ListNode(1)
-        head.next = ListNode(2)
-        head.next?.next = ListNode(3)
-        head.next?.next?.next = ListNode(4)
-        head.next?.next?.next?.next = ListNode(5)
-        var newHead = solution.removeNthFromEnd(head, 2)
-        var expected = intArrayOf(1, 2, 3, 5)
-        assertArrayEquals(expected, convertToArray(newHead))
 
-        // Example 2
-        head = ListNode(1)
-        newHead = solution.removeNthFromEnd(head, 1)
-        assertNull(newHead)
+        assertEquals("MMMDCCXLIX", solution.intToRoman(3749))
 
-        // Example 3
-        head = ListNode(1)
-        head.next = ListNode(2)
-        newHead = solution.removeNthFromEnd(head, 1)
-        expected = intArrayOf(1)
-        assertArrayEquals(expected, convertToArray(newHead))
-    }
+        assertEquals("LVIII", solution.intToRoman(58))
 
-    private fun convertToArray(head: ListNode?): IntArray {
-        val result = mutableListOf<Int>()
-        var current: ListNode? = head
-        while (current != null) {
-            result.add(current.`val`)
-            current = current.next
-        }
-        return result.toIntArray()
+        assertEquals("MCMXCIV", solution.intToRoman(1994))
+
+        assertEquals("III", solution.intToRoman(3))
+        assertEquals("CDXLIV", solution.intToRoman(444))
     }
 }
