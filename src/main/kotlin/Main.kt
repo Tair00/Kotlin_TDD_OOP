2fbe74db6fa6100cc361c1d306a1029ecca55d62
Tair00 <suleimanov.t.i.20@gmail.com>
2024-06-05 15:00:24 +0300

    minimumChairs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,5 @@
 fun main() {
     val solution = Solution()
-
-    val nums1 = intArrayOf(2,3,0,1,4)
-    val target1 = 5
-    println(solution.jump(nums1))
-    val nums2 = intArrayOf(0)
-    println(solution.jump(nums2))
-    val nums3 = intArrayOf(1,1,1,1)
-    println(solution.jump(nums3))
+    println(solution.minimumChairs("EEEEE"))
+    println(solution.minimumChairs("ELELEEL"))
 }
--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,19 +1,20 @@
 class Solution {
-    fun jump(nums: IntArray): Int {
-        if (nums.size < 2) return 0
 
-        var jumps = 0
-        var currentEnd = 0
-        var farthest = 0
+    fun minimumChairs(s: String): Int {
+        var currentPeople = 0
+        var maxPeople = 0
 
-        for (i in 0 until nums.size - 1) {
-            farthest = maxOf(farthest, i + nums[i])
-            if (i == currentEnd) {
-                jumps++
-                currentEnd = farthest
-                if (currentEnd >= nums.size - 1) break
+        for (char in s) {
+            if (char == 'E') {
+                currentPeople++
+                if (currentPeople > maxPeople) {
+                    maxPeople = currentPeople
+                }
+            } else if (char == 'L') {
+                currentPeople--
             }
         }
-        return jumps
+
+        return maxPeople
     }
 }
