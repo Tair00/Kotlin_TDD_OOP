5133337c8f510d68eb66a663a4a02a6d865abfa8
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-06 11:11:55 +0300

    passThePillow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,5 @@
 fun main() {
-
-    val node1 = ListNode(5)
-    val node2 = ListNode(3)
-    val node3 = ListNode(1)
-    val node4 = ListNode(2)
-    val node5 = ListNode(5)
-    val node6 = ListNode(1)
-    val node7 = ListNode(2)
-
-
-    node1.next = node2
-    node2.next = node3
-    node3.next = node4
-    node4.next = node5
-    node5.next = node6
-    node6.next = node7
-
-
     val solution = Solution()
-
-
-    val result = solution.nodesBetweenCriticalPoints(node1)
-
-
-    println("minDistance: ${result[0]}, maxDistance: ${result[1]}")
+    println(solution.passThePillow(4, 5)) // Output: 2
+    println(solution.passThePillow(3, 2)) // Output: 3
 }
--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,42 +1,16 @@
-
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
-
 class Solution {
-    fun nodesBetweenCriticalPoints(head: ListNode?): IntArray {
-        if (head == null || head.next == null || head.next!!.next == null) {
-            return intArrayOf(-1, -1)
-        }
+    fun passThePillow(n: Int, time: Int): Int {
 
-        var prev: ListNode? = head
-        var curr: ListNode? = head.next
-        var next: ListNode? = head.next!!.next
-        var index = 1
-        val criticalPoints = mutableListOf<Int>()
+        val fullCycles = time / (n - 1)
+        val remainingTime = time % (n - 1)
 
-        while (next != null) {
-            if ((curr!!.`val` > prev!!.`val` && curr.`val` > next.`val`) ||
-                (curr.`val` < prev.`val` && curr.`val` < next.`val`)) {
-                criticalPoints.add(index)
-            }
-            prev = curr
-            curr = next
-            next = next.next
-            index++
-        }
 
-        if (criticalPoints.size < 2) {
-            return intArrayOf(-1, -1)
-        }
+        val direction = if (fullCycles % 2 == 0) 1 else -1
 
-        var minDistance = Int.MAX_VALUE
-        var maxDistance = criticalPoints.last() - criticalPoints.first()
 
-        for (i in 1 until criticalPoints.size) {
-            minDistance = minOf(minDistance, criticalPoints[i] - criticalPoints[i - 1])
-        }
+        val initialPosition = if (direction == 1) 1 else n
 
-        return intArrayOf(minDistance, maxDistance)
+
+        return initialPosition + direction * remainingTime
     }
 }
