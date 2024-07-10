0ca0cc36a4915da3b01b17e18b2675ebebfae438
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-10 13:32:11 +0300

    minOperations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 fun main() {
     val solution = Solution()
-    val customers = arrayOf(
-        intArrayOf(1, 2),
-        intArrayOf(2, 5),
-        intArrayOf(4, 3)
-    )
-    val averageWaitingTime = solution.averageWaitingTime(customers)
-    println("Average Waiting Time: %.5f".format(averageWaitingTime))
+    val logs1 = arrayOf("d1/", "d2/", "../", "d21/", "./")
+    println(solution.minOperations(logs1)) // Output: 2
+
+    val logs2 = arrayOf("d1/", "d2/", "./", "d3/", "../", "d31/")
+    println(solution.minOperations(logs2)) // Output: 3
+
+    val logs3 = arrayOf("d1/", "../", "../", "../")
+    println(solution.minOperations(logs3)) // Output: 0
 }
--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,26 +1,13 @@
 class Solution {
-    fun averageWaitingTime(customers: Array<IntArray>): Double {
-        var currentTime = 0
-        var totalWaitingTime = 0.0
-
-        for (customer in customers) {
-            val arrivalTime = customer[0]
-            val preparationTime = customer[1]
-
-
-            currentTime = maxOf(currentTime, arrivalTime)
-
-
-            val finishTime = currentTime + preparationTime
-
-
-            val waitingTime = finishTime - arrivalTime
-            totalWaitingTime += waitingTime
-
-            currentTime = finishTime
+    fun minOperations(logs: Array<String>): Int {
+        var depth = 0
+        for (log in logs) {
+            when (log) {
+                "../" -> if (depth > 0) depth--
+                "./" -> continue
+                else -> depth++
+            }
         }
-
-
-        return totalWaitingTime / customers.size
+        return depth
     }
 }
