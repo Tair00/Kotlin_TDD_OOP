65dcec30b7562e31b40872614e8c2e446e7cac26
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-11 19:31:03 +0300

    reverseParentheses

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,6 @@
 fun main() {
     val solution = Solution()
-    val logs1 = arrayOf("d1/", "d2/", "../", "d21/", "./")
-    println(solution.minOperations(logs1)) // Output: 2
-
-    val logs2 = arrayOf("d1/", "d2/", "./", "d3/", "../", "d31/")
-    println(solution.minOperations(logs2)) // Output: 3
-
-    val logs3 = arrayOf("d1/", "../", "../", "../")
-    println(solution.minOperations(logs3)) // Output: 0
+    println(solution.reverseParentheses("(abcd)")) // Output: "dcba"
+    println(solution.reverseParentheses("(u(love)i)")) // Output: "iloveu"
+    println(solution.reverseParentheses("(ed(et(oc))el)")) // Output: "leetcode"
 }
--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,13 +1,19 @@
 class Solution {
-    fun minOperations(logs: Array<String>): Int {
-        var depth = 0
-        for (log in logs) {
-            when (log) {
-                "../" -> if (depth > 0) depth--
-                "./" -> continue
-                else -> depth++
+    fun reverseParentheses(s: String): String {
+        val stack = mutableListOf<StringBuilder>()
+        stack.add(StringBuilder())
+
+        for (char in s) {
+            when (char) {
+                '(' -> stack.add(StringBuilder())
+                ')' -> {
+                    val top = stack.removeAt(stack.size - 1)
+                    stack.last().append(top.reverse())
+                }
+                else -> stack.last().append(char)
             }
         }
-        return depth
+
+        return stack.first().toString()
     }
 }
