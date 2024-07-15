8ffb65b412c9f2e2f92b19c71eac0b6bce3b2795
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-15 10:17:45 +0300

    countOfAtoms

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
 fun main() {
     val solution = Solution()
-    val formula = "K4(ON(SO3)2)2"
-    val result = solution.countOfAtoms(formula)
-    println(result)  // Вывод: "K4N2O14S4"
+    val descriptions = arrayOf(
+        intArrayOf(20, 15, 1),
+        intArrayOf(20, 17, 0),
+        intArrayOf(50, 20, 1),
+        intArrayOf(50, 80, 0),
+        intArrayOf(80, 19, 1)
+    )
+    val root = solution.createBinaryTree(descriptions)
+    printTree(root)
+}
+
+fun printTree(root: TreeNode?) {
+    if (root == null) return
+    println(root.`val`)
+    printTree(root.left)
+    printTree(root.right)
 }
