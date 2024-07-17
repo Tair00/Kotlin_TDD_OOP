f44acb5ed2a680fcbaaa701ea484b8f898af7609
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-17 09:25:41 +0300

    delNodes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,31 @@
 fun main() {
-
-    val root = TreeNode(5)
-    root.left = TreeNode(1)
-    root.right = TreeNode(2)
-    root.left?.left = TreeNode(3)
-    root.left?.right = TreeNode(4)
-
-
-    val startValue = 3
-    val destValue = 4
-
+    // Example 1
+    val root1 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            left = TreeNode(4)
+            right = TreeNode(5)
+        }
+        right = TreeNode(3).apply {
+            left = TreeNode(6)
+            right = TreeNode(7)
+        }
+    }
+    val toDelete1 = intArrayOf(3, 5)
 
     val solution = Solution()
-    val result = solution.getDirections(root, startValue, destValue)
+    val forest1 = solution.delNodes(root1, toDelete1)
+    println(forest1.map { it?.`val` })  // Expected output: [1, 6, 7]
 
+    // Example 2
+    val root2 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            right = TreeNode(4).apply {
+                left = TreeNode(3)
+            }
+        }
+    }
+    val toDelete2 = intArrayOf(3)
 
-    println(result)
+    val forest2 = solution.delNodes(root2, toDelete2)
+    println(forest2.map { it?.`val` })  // Expected output: [1]
 }
