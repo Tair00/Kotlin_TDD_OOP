2e9cd24d1cd6c9521a3371fb34944d018638778b
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-18 10:22:41 +0300

    countPairs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
 fun main() {
-    // Example 1
+
     val root1 = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            right = TreeNode(4)
+        }
+        right = TreeNode(3)
+    }
+    val distance1 = 3
+
+
+    val root2 = TreeNode(1).apply {
         left = TreeNode(2).apply {
             left = TreeNode(4)
             right = TreeNode(5)
@@ -10,22 +19,25 @@ fun main() {
             right = TreeNode(7)
         }
     }
-    val toDelete1 = intArrayOf(3, 5)
+    val distance2 = 3
 
-    val solution = Solution()
-    val forest1 = solution.delNodes(root1, toDelete1)
-    println(forest1.map { it?.`val` })  // Expected output: [1, 6, 7]
 
-    // Example 2
-    val root2 = TreeNode(1).apply {
-        left = TreeNode(2).apply {
-            right = TreeNode(4).apply {
-                left = TreeNode(3)
+    val root3 = TreeNode(7).apply {
+        left = TreeNode(1).apply {
+            left = TreeNode(6)
+        }
+        right = TreeNode(4).apply {
+            left = TreeNode(5)
+            right = TreeNode(3).apply {
+                right = TreeNode(2)
             }
         }
     }
-    val toDelete2 = intArrayOf(3)
+    val distance3 = 3
 
-    val forest2 = solution.delNodes(root2, toDelete2)
-    println(forest2.map { it?.`val` })  // Expected output: [1]
+    val solution = Solution()
+
+    println("Example 1: ${solution.countPairs(root1, distance1)}") // Output: 1
+    println("Example 2: ${solution.countPairs(root2, distance2)}") // Output: 2
+    println("Example 3: ${solution.countPairs(root3, distance3)}") // Output: 1
 }
