7af9804b3588f6e3fda27c750b61bf3da04930c2
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-19 09:40:38 +0300

    luckyNumbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,24 @@
 fun main() {
-
-    val root1 = TreeNode(1).apply {
-        left = TreeNode(2).apply {
-            right = TreeNode(4)
-        }
-        right = TreeNode(3)
-    }
-    val distance1 = 3
-
-
-    val root2 = TreeNode(1).apply {
-        left = TreeNode(2).apply {
-            left = TreeNode(4)
-            right = TreeNode(5)
-        }
-        right = TreeNode(3).apply {
-            left = TreeNode(6)
-            right = TreeNode(7)
-        }
-    }
-    val distance2 = 3
-
-
-    val root3 = TreeNode(7).apply {
-        left = TreeNode(1).apply {
-            left = TreeNode(6)
-        }
-        right = TreeNode(4).apply {
-            left = TreeNode(5)
-            right = TreeNode(3).apply {
-                right = TreeNode(2)
-            }
-        }
-    }
-    val distance3 = 3
-
     val solution = Solution()
 
-    println("Example 1: ${solution.countPairs(root1, distance1)}") // Output: 1
-    println("Example 2: ${solution.countPairs(root2, distance2)}") // Output: 2
-    println("Example 3: ${solution.countPairs(root3, distance3)}") // Output: 1
+    val matrix1 = arrayOf(
+        intArrayOf(3, 7, 8),
+        intArrayOf(9, 11, 13),
+        intArrayOf(15, 16, 17)
+    )
+
+    val matrix2 = arrayOf(
+        intArrayOf(1, 10, 4, 2),
+        intArrayOf(9, 3, 8, 7),
+        intArrayOf(15, 16, 17, 12)
+    )
+
+    val matrix3 = arrayOf(
+        intArrayOf(7, 8),
+        intArrayOf(1, 2)
+    )
+
+    println(solution.luckyNumbers(matrix1)) // Output: [15]
+    println(solution.luckyNumbers(matrix2)) // Output: [12]
+    println(solution.luckyNumbers(matrix3)) // Output: [7]
 }
