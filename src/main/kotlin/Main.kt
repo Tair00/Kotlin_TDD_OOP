fb3397a0ab93afb66d68ad994b827b98b9be107c
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-21 14:44:02 +0300

    buildMatrix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,18 @@
 fun main() {
     val solution = Solution()
 
-    val rowSum1 = intArrayOf(3, 8)
-    val colSum1 = intArrayOf(4, 7)
-    val matrix1 = solution.restoreMatrix(rowSum1, colSum1)
-    println("Матрица для rowSum1 и colSum1:")
+    val k1 = 3
+    val rowConditions1 = arrayOf(
+        intArrayOf(1, 2),
+        intArrayOf(3, 2)
+    )
+    val colConditions1 = arrayOf(
+        intArrayOf(2, 1),
+        intArrayOf(3, 2)
+    )
+    val matrix1 = solution.buildMatrix(k1, rowConditions1, colConditions1)
+    println("Матрица для k1:")
     for (row in matrix1) {
-        println(row.joinToString(", "))
-    }
-
-    val rowSum2 = intArrayOf(5, 7, 10)
-    val colSum2 = intArrayOf(8, 6, 8)
-    val matrix2 = solution.restoreMatrix(rowSum2, colSum2)
-    println("\nМатрица для rowSum2 и colSum2:")
-    for (row in matrix2) {
-        println(row.joinToString(", "))
-    }
-
-    val rowSum3 = intArrayOf(14, 9, 4)
-    val colSum3 = intArrayOf(6, 8, 7, 6)
-    val matrix3 = solution.restoreMatrix(rowSum3, colSum3)
-    println("\nМатрица для rowSum3 и colSum3:")
-    for (row in matrix3) {
         println(row.joinToString(", "))
     }
 }
