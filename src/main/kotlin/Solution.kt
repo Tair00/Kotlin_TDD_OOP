c2365cf8208591d3469ffcaf67e11e08f5a3ef4f
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-22 10:19:39 +0300

    sortPeople

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,60 +1,15 @@
 class Solution {
-    fun buildMatrix(k: Int, rowConditions: Array<IntArray>, colConditions: Array<IntArray>): Array<IntArray> {
-        val rowOrder = topologicalSort(k, rowConditions)
-        val colOrder = topologicalSort(k, colConditions)
+    fun sortPeople(names: Array<String>, heights: IntArray): Array<String> {
 
-        if (rowOrder.isEmpty() || colOrder.isEmpty()) {
-            return arrayOf()
+        val nameHeightPairs = mutableListOf<Pair<String, Int>>()
+        for (i in names.indices) {
+            nameHeightPairs.add(names[i] to heights[i])
         }
 
-        val matrix = Array(k) { IntArray(k) }
-        val position = Array(k + 1) { IntArray(2) }
+        nameHeightPairs.sortByDescending { it.second }
 
-        for (i in rowOrder.indices) {
-            position[rowOrder[i]][0] = i
-        }
+        val sortedNames = nameHeightPairs.map { it.first }.toTypedArray()
 
-        for (i in colOrder.indices) {
-            position[colOrder[i]][1] = i
-        }
-
-        for (i in 1..k) {
-            matrix[position[i][0]][position[i][1]] = i
-        }
-
-        return matrix
-    }
-
-    private fun topologicalSort(k: Int, conditions: Array<IntArray>): List<Int> {
-        val graph = Array(k + 1) { mutableListOf<Int>() }
-        val inDegree = IntArray(k + 1)
-
-        for (condition in conditions) {
-            val u = condition[0]
-            val v = condition[1]
-            graph[u].add(v)
-            inDegree[v]++
-        }
-
-        val queue = ArrayDeque<Int>()
-        for (i in 1..k) {
-            if (inDegree[i] == 0) {
-                queue.add(i)
-            }
-        }
-
-        val order = mutableListOf<Int>()
-        while (queue.isNotEmpty()) {
-            val node = queue.removeFirst()
-            order.add(node)
-            for (neighbor in graph[node]) {
-                inDegree[neighbor]--
-                if (inDegree[neighbor] == 0) {
-                    queue.add(neighbor)
-                }
-            }
-        }
-
-        return if (order.size == k) order else emptyList()
+        return sortedNames
     }
 }
