84fc126e825266ce1b7258dab84191108d7058fa
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-29 11:14:44 +0300

    numTeams

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,15 @@
 fun main() {
     val solution = Solution()
 
-    // Пример входных данных
-    val n = 5
-    val edges = arrayOf(
-        intArrayOf(1, 2),
-        intArrayOf(1, 3),
-        intArrayOf(2, 4),
-        intArrayOf(3, 4),
-        intArrayOf(4, 5)
-    )
-    val time = 3
-    val change = 5
+    // Пример 1
+    val rating1 = intArrayOf(2, 5, 3, 4, 1)
+    println("Output for rating1: ${solution.numTeams(rating1)}")  // Ожидаемый результат: 3
 
-    // Вызов метода secondMinimum
-    val result = solution.secondMinimum(n, edges, time, change)
+    // Пример 2
+    val rating2 = intArrayOf(2, 1, 3)
+    println("Output for rating2: ${solution.numTeams(rating2)}")  // Ожидаемый результат: 0
 
-    // Вывод результата
-    println("Второе минимальное время: $result")
+    // Пример 3
+    val rating3 = intArrayOf(1, 2, 3, 4)
+    println("Output for rating3: ${solution.numTeams(rating3)}")  // Ожидаемый результат: 4
 }
