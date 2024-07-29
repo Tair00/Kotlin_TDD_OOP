a17a396d236e43a7e230ffe27d416d9cc212fc47
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-29 11:14:28 +0300

    numTeams

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,44 +1,27 @@
-import java.util.PriorityQueue
-
 class Solution {
-    data class State(val node: Int, val time: Int): Comparable<State> {
-        override fun compareTo(other: State): Int = this.time - other.time
-    }
+    fun numTeams(rating: IntArray): Int {
+        var count = 0
+        val n = rating.size
 
-    fun secondMinimum(n: Int, edges: Array<IntArray>, time: Int, change: Int): Int {
-        val graph = Array(n + 1) { mutableListOf<Int>() }
-        for (edge in edges) {
-            graph[edge[0]].add(edge[1])
-            graph[edge[1]].add(edge[0])
-        }
+        for (j in 1 until n - 1) {
+            var lessLeft = 0
+            var greaterLeft = 0
+            var lessRight = 0
+            var greaterRight = 0
 
-        val arrivalTimes = Array(n + 1) { IntArray(2) { Int.MAX_VALUE } }
-        arrivalTimes[1][0] = 0
-
-        val pq = PriorityQueue<State>()
-        pq.add(State(1, 0))
-
-        while (pq.isNotEmpty()) {
-            val (node, currentTime) = pq.poll()
-
-            for (neighbor in graph[node]) {
-                var travelTime = currentTime
-                if ((travelTime / change) % 2 == 1) {
-                    travelTime += change - (travelTime % change)
-                }
-                travelTime += time
-
-                if (travelTime < arrivalTimes[neighbor][0]) {
-                    arrivalTimes[neighbor][1] = arrivalTimes[neighbor][0]
-                    arrivalTimes[neighbor][0] = travelTime
-                    pq.add(State(neighbor, travelTime))
-                } else if (travelTime > arrivalTimes[neighbor][0] && travelTime < arrivalTimes[neighbor][1]) {
-                    arrivalTimes[neighbor][1] = travelTime
-                    pq.add(State(neighbor, travelTime))
-                }
+            for (i in 0 until j) {
+                if (rating[i] < rating[j]) lessLeft++
+                if (rating[i] > rating[j]) greaterLeft++
             }
+
+            for (k in j + 1 until n) {
+                if (rating[k] < rating[j]) lessRight++
+                if (rating[k] > rating[j]) greaterRight++
+            }
+
+            count += lessLeft * greaterRight + greaterLeft * lessRight
         }
 
-        return if (arrivalTimes[n][1] == Int.MAX_VALUE) -1 else arrivalTimes[n][1]
+        return count
     }
 }
