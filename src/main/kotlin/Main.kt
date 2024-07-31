f3fdec7a556937d3a3a7dc8bea1686e58ce1ee0e
Tair00 <suleimanov.t.i.20@gmail.com>
2024-07-31 19:22:17 +0300

    minHeightShelves

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
 fun main() {
     val solution = Solution()
-    println(solution.minimumDeletions("aababbab")) // Ожидаемый результат: 2
-    println(solution.minimumDeletions("bbaaaaabb")) // Ожидаемый результат: 2
+    val books = arrayOf(
+        intArrayOf(1, 3), // книга 1: толщина 1, высота 3
+        intArrayOf(2, 4), // книга 2: толщина 2, высота 4
+        intArrayOf(3, 2), // книга 3: толщина 3, высота 2
+        intArrayOf(4, 5)  // книга 4: толщина 4, высота 5
+    )
+    val shelfWidth = 6
 
+    val result = solution.minHeightShelves(books, shelfWidth)
+    println(result)
 }
