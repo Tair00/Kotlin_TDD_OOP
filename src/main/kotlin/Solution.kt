225cfa355647690a228d97821719544afa1c1748
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-01 11:58:26 +0300

    minHeightShelves

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,20 +1,12 @@
 class Solution {
-    fun minHeightShelves(books: Array<IntArray>, shelfWidth: Int): Int {
-        val n = books.size
-        val dp = IntArray(n + 1) { Int.MAX_VALUE }
-        dp[0] = 0
-
-        for (i in 1..n) {
-            var width = 0
-            var height = 0
-            for (j in i downTo 1) {
-                width += books[j - 1][0]
-                if (width > shelfWidth) break
-                height = maxOf(height, books[j - 1][1])
-                dp[i] = minOf(dp[i], dp[j - 1] + height)
+    fun countSeniors(details: Array<String>): Int {
+        var count = 0
+        for (detail in details) {
+            val age = detail.substring(11, 13).toInt()
+            if (age > 60) {
+                count++
             }
         }
-
-        return dp[n]
+        return count
     }
 }
