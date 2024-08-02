e90079d92ff6a91c6036aaafbc9d28d1f2334814
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-02 15:05:07 +0300

    minSwaps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 fun main() {
         val solution = Solution()
-        val details1 = arrayOf("7868190130M7522", "5303914400F9211", "9273338290F4010")
-        println(solution.countSeniors(details1)) // Ожидаемый результат: 2
+        val nums1 = intArrayOf(0, 1, 0, 1, 1, 0, 0)
+        println(solution.minSwaps(nums1)) // Ожидаемый результат: 1
 
-        val details2 = arrayOf("1313579440F2036", "2921522980M5644")
-        println(solution.countSeniors(details2)) // Ожидаемый результат: 0
+        val nums2 = intArrayOf(0, 1, 1, 1, 0, 0, 1, 1, 0)
+        println(solution.minSwaps(nums2)) // Ожидаемый результат: 2
+
+        val nums3 = intArrayOf(1, 1, 0, 0, 1)
+        println(solution.minSwaps(nums3)) // Ожидаемый результат: 0
 }
