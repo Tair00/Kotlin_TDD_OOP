be877a66bfd5321b7f07b044e1fc5bbfd9184391
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-04 12:40:32 +0300

    rangeSum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,24 @@
 fun main() {
         val solution = Solution()
-        val target1 = intArrayOf(1, 2, 3, 4)
-        val arr1 = intArrayOf(2, 4, 1, 3)
-        println(solution.canBeEqual(target1, arr1)) // Ожидаемый результат: true
 
-        val target2 = intArrayOf(7)
-        val arr2 = intArrayOf(7)
-        println(solution.canBeEqual(target2, arr2)) // Ожидаемый результат: true
 
-        val target3 = intArrayOf(3, 7, 9)
-        val arr3 = intArrayOf(3, 7, 11)
-        println(solution.canBeEqual(target3, arr3)) // Ожидаемый результат: false
+        val nums1 = intArrayOf(1, 2, 3, 4)
+        val n1 = nums1.size
+        val left1 = 1
+        val right1 = 5
+        println(solution.rangeSum(nums1, n1, left1, right1)) // Ожидаемый результат: 13
+
+
+        val nums2 = intArrayOf(1, 2, 3, 4)
+        val n2 = nums2.size
+        val left2 = 3
+        val right2 = 4
+        println(solution.rangeSum(nums2, n2, left2, right2)) // Ожидаемый результат: 6
+
+
+        val nums3 = intArrayOf(1, 2, 3, 4)
+        val n3 = nums3.size
+        val left3 = 1
+        val right3 = 10
+        println(solution.rangeSum(nums3, n3, left3, right3)) // Ожидаемый результат: 50
 }
