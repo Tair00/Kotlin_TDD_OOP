a62c39a5c4f748116c3ade24ce5866716424e4f4
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-04 12:39:39 +0300

    rangeSum

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,8 +1,26 @@
 class Solution {
-    fun canBeEqual(target: IntArray, arr: IntArray): Boolean {
-        // Сортируем оба массива и проверяем их на равенство
-        target.sort()
-        arr.sort()
-        return target.contentEquals(arr)
+    fun rangeSum(nums: IntArray, n: Int, left: Int, right: Int): Int {
+        val mod = 1_000_000_007
+        val sums = mutableListOf<Int>()
+
+        // Создание всех возможных сумм подмассивов
+        for (i in nums.indices) {
+            var currentSum = 0
+            for (j in i until nums.size) {
+                currentSum += nums[j]
+                sums.add(currentSum)
+            }
+        }
+
+        // Сортировка сумм
+        sums.sort()
+
+        // Подсчет суммы элементов от left до right
+        var result = 0
+        for (i in (left - 1) until right) {
+            result = (result + sums[i]) % mod
+        }
+
+        return result
     }
 }
