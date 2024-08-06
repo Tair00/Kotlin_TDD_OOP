7bea1e68703f85f9f6562d2228e53922285fe7f9
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-06 13:41:12 +0300

    minimumPushes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,6 @@
 fun main() {
         val solution = Solution()
-
-        // Пример 1
-        val arr1 = arrayOf("d", "b", "c", "b", "c", "a")
-        val k1 = 2
-        println(solution.kthDistinct(arr1, k1)) // Ожидаемый результат: "a"
-
-        // Пример 2
-        val arr2 = arrayOf("aaa", "aa", "a")
-        val k2 = 1
-        println(solution.kthDistinct(arr2, k2)) // Ожидаемый результат: "aaa"
-
-        // Пример 3
-        val arr3 = arrayOf("a", "b", "a")
-        val k3 = 3
-        println(solution.kthDistinct(arr3, k3)) // Ожидаемый результат: ""
+        println(solution.minimumPushes("abcde"))           // Ожидаемый результат: 5
+        println(solution.minimumPushes("xyzxyzxyzxyz"))    // Ожидаемый результат: 12
+        println(solution.minimumPushes("aabbccddeeffgghhiiiiii")) // Ожидаемый результат: 24
 }
