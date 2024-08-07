f4020b4fd92d85349c6a90113182e431e2d1814b
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-07 11:31:14 +0300

    numberToWords

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,29 +1,42 @@
 class Solution {
-    fun minimumPushes(word: String): Int {
-        val frequencyMap = mutableMapOf<Char, Int>()
+    private val belowTwenty = arrayOf(
+        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+        "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    )
+    private val tens = arrayOf(
+        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    )
+    private val thousands = arrayOf(
+        "", "Thousand", "Million", "Billion"
+    )
 
+    fun numberToWords(num: Int): String {
+        if (num == 0) return "Zero"
 
-        for (c in word) {
-            frequencyMap[c] = frequencyMap.getOrDefault(c, 0) + 1
-        }
+        var num = num
+        var result = ""
+        var i = 0
 
-        val frequencyList = mutableListOf<Pair<Char, Int>>()
-        for ((char, freq) in frequencyMap) {
-            frequencyList.add(Pair(char, freq))
-        }
-
-        frequencyList.sortByDescending { it.second }
-
-        var ans = 0
-        var i = 1
-
-        for (j in frequencyList.indices) {
-            if (j > 7 && j % 8 == 0) {
-                i++
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                result = helper(num % 1000) + thousands[i] + " " + result
             }
-            ans += i * frequencyList[j].second
+            num /= 1000
+            i++
         }
 
-        return ans
+        return result.trim()
+    }
+
+    private fun helper(num: Int): String {
+        if (num == 0) {
+            return ""
+        } else if (num < 20) {
+            return belowTwenty[num] + " "
+        } else if (num < 100) {
+            return tens[num / 10] + " " + helper(num % 10)
+        } else {
+            return belowTwenty[num / 100] + " Hundred " + helper(num % 100)
+        }
     }
 }
