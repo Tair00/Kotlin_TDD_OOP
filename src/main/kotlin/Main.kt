4d94272bf183055fd8e8aa4b012e2e42aeafa9e6
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-10 10:55:17 +0300

    regionsBySlashes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,11 @@
 fun main() {
-        val solution = Solution()
-        val grid1 = arrayOf(
-                intArrayOf(4, 3, 8, 4),
-                intArrayOf(9, 5, 1, 9),
-                intArrayOf(2, 7, 6, 2)
-        )
-        println(solution.numMagicSquaresInside(grid1))  // Ожидаемый результат: 1
+        val grid1 = arrayOf(" /", "/ ")
+        val grid2 = arrayOf(" /", "  ")
+        val grid3 = arrayOf("/\\", "\\/")
 
-        val grid2 = arrayOf(
-                intArrayOf(8)
-        )
-        println(solution.numMagicSquaresInside(grid2))  // Ожидаемый результат: 0
+        val solution = Solution()
+
+        println("Number of regions in grid1: " + solution.regionsBySlashes(grid1)) // Ожидаемый результат: 2
+        println("Number of regions in grid2: " + solution.regionsBySlashes(grid2)) // Ожидаемый результат: 1
+        println("Number of regions in grid3: " + solution.regionsBySlashes(grid3)) // Ожидаемый результат: 5
 }
