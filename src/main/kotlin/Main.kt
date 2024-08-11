fc16e7e38fb450d198ac8d9b263230479ef709e9
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-11 19:40:29 +0300

    minDays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,33 @@
 fun main() {
-        val grid1 = arrayOf(" /", "/ ")
-        val grid2 = arrayOf(" /", "  ")
-        val grid3 = arrayOf("/\\", "\\/")
-
         val solution = Solution()
 
-        println("Number of regions in grid1: " + solution.regionsBySlashes(grid1)) // Ожидаемый результат: 2
-        println("Number of regions in grid2: " + solution.regionsBySlashes(grid2)) // Ожидаемый результат: 1
-        println("Number of regions in grid3: " + solution.regionsBySlashes(grid3)) // Ожидаемый результат: 5
+
+        val grid1 = arrayOf(
+                intArrayOf(0, 1, 1, 0),
+                intArrayOf(0, 1, 1, 0),
+                intArrayOf(0, 0, 0, 0)
+        )
+        println("Minimum days to disconnect grid1: " + solution.minDays(grid1)) // Ожидаемый результат: 2
+
+
+        val grid2 = arrayOf(
+                intArrayOf(1, 1)
+        )
+        println("Minimum days to disconnect grid2: " + solution.minDays(grid2)) // Ожидаемый результат: 2
+
+
+        val grid3 = arrayOf(
+                intArrayOf(0, 0, 0),
+                intArrayOf(0, 1, 0),
+                intArrayOf(0, 0, 0)
+        )
+        println("Minimum days to disconnect grid3: " + solution.minDays(grid3)) // Ожидаемый результат: 1
+
+
+        val grid4 = arrayOf(
+                intArrayOf(1, 0, 1),
+                intArrayOf(0, 1, 0),
+                intArrayOf(1, 0, 1)
+        )
+        println("Minimum days to disconnect grid4: " + solution.minDays(grid4)) // Ожидаемый результат: 1
 }
