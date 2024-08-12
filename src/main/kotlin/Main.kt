149bbe7dc7283241b3f6234fd9de40d17ac7bf55
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-12 16:17:43 +0300

    KthLargest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,8 @@
 fun main() {
-        val solution = Solution()
-
-
-        val grid1 = arrayOf(
-                intArrayOf(0, 1, 1, 0),
-                intArrayOf(0, 1, 1, 0),
-                intArrayOf(0, 0, 0, 0)
-        )
-        println("Minimum days to disconnect grid1: " + solution.minDays(grid1)) // Ожидаемый результат: 2
-
-
-        val grid2 = arrayOf(
-                intArrayOf(1, 1)
-        )
-        println("Minimum days to disconnect grid2: " + solution.minDays(grid2)) // Ожидаемый результат: 2
-
-
-        val grid3 = arrayOf(
-                intArrayOf(0, 0, 0),
-                intArrayOf(0, 1, 0),
-                intArrayOf(0, 0, 0)
-        )
-        println("Minimum days to disconnect grid3: " + solution.minDays(grid3)) // Ожидаемый результат: 1
-
-
-        val grid4 = arrayOf(
-                intArrayOf(1, 0, 1),
-                intArrayOf(0, 1, 0),
-                intArrayOf(1, 0, 1)
-        )
-        println("Minimum days to disconnect grid4: " + solution.minDays(grid4)) // Ожидаемый результат: 1
+        val kthLargest = KthLargest(3, intArrayOf(4, 5, 8, 2))
+        println(kthLargest.add(3))   // Outputs 4
+        println(kthLargest.add(5))   // Outputs 5
+        println(kthLargest.add(10))  // Outputs 5
+        println(kthLargest.add(9))   // Outputs 8
+        println(kthLargest.add(4))   // Outputs 8
 }
