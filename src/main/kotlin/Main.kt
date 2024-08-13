af0c19de94f85afeb93543db67954e3f68e13102
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-13 13:15:59 +0300

    combinationSum2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 fun main() {
-        val kthLargest = KthLargest(3, intArrayOf(4, 5, 8, 2))
-        println(kthLargest.add(3))   // Outputs 4
-        println(kthLargest.add(5))   // Outputs 5
-        println(kthLargest.add(10))  // Outputs 5
-        println(kthLargest.add(9))   // Outputs 8
-        println(kthLargest.add(4))   // Outputs 8
+        val solution = Solution()
+        val candidates1 = intArrayOf(10,1,2,7,6,1,5)
+        val target1 = 8
+        println(solution.combinationSum2(candidates1, target1))  // Ожидаемый результат: [[1,1,6], [1,2,5], [1,7], [2,6]]
+
+        val candidates2 = intArrayOf(2,5,2,1,2)
+        val target2 = 5
+        println(solution.combinationSum2(candidates2, target2))  // Ожидаемый результат: [[1,2,2], [5]]
 }
