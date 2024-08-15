ae8a5e3eb4e2f8f35b56905e09c406ee4adce6a5
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-15 08:39:31 +0300

    lemonadeChange

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,29 +1,31 @@
 class Solution {
-    fun smallestDistancePair(nums: IntArray, k: Int): Int {
-        nums.sort()
+    fun lemonadeChange(bills: IntArray): Boolean {
+        var fiveCount = 0
+        var tenCount = 0
 
-        var low = 0
-        var high = nums[nums.size - 1] - nums[0]
-
-        while (low < high) {
-            val mid = low + (high - low) / 2
-            var count = 0
-            var left = 0
-
-            for (right in nums.indices) {
-                while (nums[right] - nums[left] > mid) {
-                    left++
+        for (bill in bills) {
+            when (bill) {
+                5 -> fiveCount++
+                10 -> {
+                    if (fiveCount == 0) {
+                        return false
+                    }
+                    fiveCount--
+                    tenCount++
                 }
-                count += right - left
-            }
-
-            if (count >= k) {
-                high = mid
-            } else {
-                low = mid + 1
+                20 -> {
+                    if (tenCount > 0 && fiveCount > 0) {
+                        tenCount--
+                        fiveCount--
+                    } else if (fiveCount >= 3) {
+                        fiveCount -= 3
+                    } else {
+                        return false
+                    }
+                }
             }
         }
 
-        return low
+        return true
     }
 }
