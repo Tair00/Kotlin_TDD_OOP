d154e794a50ecfa29bfde5d47419c3dd21298068
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-16 09:55:19 +0300

    maxDistance

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,31 +1,21 @@
 class Solution {
-    fun lemonadeChange(bills: IntArray): Boolean {
-        var fiveCount = 0
-        var tenCount = 0
+    fun maxDistance(arrays: List<List<Int>>): Int {
+        var maxDist = 0
+        var minVal = arrays[0][0]
+        var maxVal = arrays[0][arrays[0].size - 1]
 
-        for (bill in bills) {
-            when (bill) {
-                5 -> fiveCount++
-                10 -> {
-                    if (fiveCount == 0) {
-                        return false
-                    }
-                    fiveCount--
-                    tenCount++
-                }
-                20 -> {
-                    if (tenCount > 0 && fiveCount > 0) {
-                        tenCount--
-                        fiveCount--
-                    } else if (fiveCount >= 3) {
-                        fiveCount -= 3
-                    } else {
-                        return false
-                    }
-                }
-            }
+        for (i in 1 until arrays.size) {
+            val currentMin = arrays[i][0]
+            val currentMax = arrays[i][arrays[i].size - 1]
+
+            // Calculate the possible distances
+            maxDist = maxOf(maxDist, Math.abs(currentMax - minVal), Math.abs(maxVal - currentMin))
+
+            // Update the global min and max
+            minVal = minOf(minVal, currentMin)
+            maxVal = maxOf(maxVal, currentMax)
         }
 
-        return true
+        return maxDist
     }
 }
