4acdcd998b3ec678046545d69b4555cc621a341d
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-18 08:44:58 +0300

    nthUglyNumber

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
 fun main() {
         val solution = Solution()
 
-        // Пример матрицы points
-        val points = arrayOf(
-                intArrayOf(1, 2, 3),
-                intArrayOf(1, 5, 1),
-                intArrayOf(3, 1, 1)
-        )
+        // Пример вызова функции nthUglyNumber
+        val n = 10
+        val result = solution.nthUglyNumber(n)
 
-        // Вызов функции maxPoints и вывод результата
-        val result = solution.maxPoints(points)
-        println("Максимальное количество очков: $result")
+        println("n-ое уродливое число для n = $n: $result")
 }
