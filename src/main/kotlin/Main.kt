c41741a871e894191af77bce79eaf31cdb7a4bcb
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-22 18:30:21 +0300

    findComplement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 fun main() {
         val solution = Solution()
 
-        // Пример вызова функции nthUglyNumber
-        val n = 10
-        val result = solution.nthUglyNumber(n)
+        val num1 = 5
+        val result1 = solution.findComplement(num1)
+        println("Complement of $num1 is $result1")  // Ожидается вывод: Complement of 5 is 2
 
-        println("n-ое уродливое число для n = $n: $result")
+        val num2 = 1
+        val result2 = solution.findComplement(num2)
+        println("Complement of $num2 is $result2")  // Ожидается вывод: Complement of 1 is 0
 }
