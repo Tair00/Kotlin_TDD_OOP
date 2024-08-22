2a424f00736b600d18b1c18929c794c130be7682
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-22 18:30:09 +0300

    findComplement

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,33 +1,19 @@
 class Solution {
-    fun nthUglyNumber(n: Int): Int {
-        val uglyNumbers = IntArray(n)
-        uglyNumbers[0] = 1
-        var i2 = 0
-        var i3 = 0
-        var i5 = 0
+    fun findComplement(num: Int): Int {
 
-        var nextMultipleOf2 = 2
-        var nextMultipleOf3 = 3
-        var nextMultipleOf5 = 5
+        val mask = (1 shl num.bitLength()) - 1
 
-        for (i in 1 until n) {
-            val nextUgly = minOf(nextMultipleOf2, nextMultipleOf3, nextMultipleOf5)
-            uglyNumbers[i] = nextUgly
 
-            if (nextUgly == nextMultipleOf2) {
-                i2++
-                nextMultipleOf2 = uglyNumbers[i2] * 2
-            }
-            if (nextUgly == nextMultipleOf3) {
-                i3++
-                nextMultipleOf3 = uglyNumbers[i3] * 3
-            }
-            if (nextUgly == nextMultipleOf5) {
-                i5++
-                nextMultipleOf5 = uglyNumbers[i5] * 5
-            }
-        }
-
-        return uglyNumbers[n - 1]
+        return num xor mask
     }
+}
+
+fun Int.bitLength(): Int {
+    var bits = 0
+    var n = this
+    while (n != 0) {
+        bits++
+        n = n shr 1
+    }
+    return bits
 }
