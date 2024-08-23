c847a865d7ffb5879fbc28ef7e7c3c3c09fe98c7
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-23 20:13:49 +0300

    fractionAddition

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,19 +1,50 @@
+import kotlin.math.abs
+
 class Solution {
-    fun findComplement(num: Int): Int {
+    fun fractionAddition(expression: String): String {
+        var numerator = 0
+        var denominator = 1
+        var index = 0
 
-        val mask = (1 shl num.bitLength()) - 1
+        while (index < expression.length) {
+
+            var sign = 1
+            if (expression[index] == '-' || expression[index] == '+') {
+                if (expression[index] == '-') sign = -1
+                index++
+            }
 
 
-        return num xor mask
+            var currentNumerator = 0
+            while (index < expression.length && expression[index].isDigit()) {
+                currentNumerator = currentNumerator * 10 + (expression[index] - '0')
+                index++
+            }
+            currentNumerator *= sign
+
+
+            index++
+
+            var currentDenominator = 0
+            while (index < expression.length && expression[index].isDigit()) {
+                currentDenominator = currentDenominator * 10 + (expression[index] - '0')
+                index++
+            }
+
+
+            numerator = numerator * currentDenominator + currentNumerator * denominator
+            denominator *= currentDenominator
+
+            val gcd = gcd(abs(numerator), denominator)
+            numerator /= gcd
+            denominator /= gcd
+        }
+
+        return "$numerator/$denominator"
     }
-}
 
-fun Int.bitLength(): Int {
-    var bits = 0
-    var n = this
-    while (n != 0) {
-        bits++
-        n = n shr 1
+    private fun gcd(a: Int, b: Int): Int {
+        if (b == 0) return a
+        return gcd(b, a % b)
     }
-    return bits
 }
