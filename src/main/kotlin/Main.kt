288c04d487076296bf055b57e3a0bf356a6d18c1
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-23 20:14:12 +0300

    fractionAddition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 fun main() {
         val solution = Solution()
 
-        val num1 = 5
-        val result1 = solution.findComplement(num1)
-        println("Complement of $num1 is $result1")  // Ожидается вывод: Complement of 5 is 2
+        val expression1 = "-1/2+1/2"
+        println(solution.fractionAddition(expression1))  // Output: "0/1"
 
-        val num2 = 1
-        val result2 = solution.findComplement(num2)
-        println("Complement of $num2 is $result2")  // Ожидается вывод: Complement of 1 is 0
+        val expression2 = "-1/2+1/2+1/3"
+        println(solution.fractionAddition(expression2))  // Output: "1/3"
+
+        val expression3 = "1/3-1/2"
+        println(solution.fractionAddition(expression3))  // Output: "-1/6"
 }
