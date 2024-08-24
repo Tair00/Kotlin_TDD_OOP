9cecc20ecaa48d76bd9d29d9ba49b25b01129e95
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-24 11:53:21 +0300

    nearestPalindromic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,28 @@
 fun main() {
         val solution = Solution()
 
-        val expression1 = "-1/2+1/2"
-        println(solution.fractionAddition(expression1))  // Output: "0/1"
+        // Пример 1
+        val n1 = "123"
+        println("Input: $n1")
+        println("Output: ${solution.nearestPalindromic(n1)}")  // Ожидается "121"
 
-        val expression2 = "-1/2+1/2+1/3"
-        println(solution.fractionAddition(expression2))  // Output: "1/3"
+        // Пример 2
+        val n2 = "1"
+        println("Input: $n2")
+        println("Output: ${solution.nearestPalindromic(n2)}")  // Ожидается "0"
 
-        val expression3 = "1/3-1/2"
-        println(solution.fractionAddition(expression3))  // Output: "-1/6"
+        // Пример 3
+        val n3 = "999"
+        println("Input: $n3")
+        println("Output: ${solution.nearestPalindromic(n3)}")  // Ожидается "1001"
+
+        // Пример 4
+        val n4 = "10"
+        println("Input: $n4")
+        println("Output: ${solution.nearestPalindromic(n4)}")  // Ожидается "9"
+
+        // Пример 5
+        val n5 = "808"
+        println("Input: $n5")
+        println("Output: ${solution.nearestPalindromic(n5)}")  // Ожидается "797"
 }
