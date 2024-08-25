038652036e9583c9e493d2dc7e966e81cbec44ce
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-25 17:03:45 +0300

    postorderTraversal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,9 @@
 fun main() {
+        val root = TreeNode(1)
+        root.right = TreeNode(2)
+        root.right?.left = TreeNode(3)
+
         val solution = Solution()
-
-        // Пример 1
-        val n1 = "123"
-        println("Input: $n1")
-        println("Output: ${solution.nearestPalindromic(n1)}")  // Ожидается "121"
-
-        // Пример 2
-        val n2 = "1"
-        println("Input: $n2")
-        println("Output: ${solution.nearestPalindromic(n2)}")  // Ожидается "0"
-
-        // Пример 3
-        val n3 = "999"
-        println("Input: $n3")
-        println("Output: ${solution.nearestPalindromic(n3)}")  // Ожидается "1001"
-
-        // Пример 4
-        val n4 = "10"
-        println("Input: $n4")
-        println("Output: ${solution.nearestPalindromic(n4)}")  // Ожидается "9"
-
-        // Пример 5
-        val n5 = "808"
-        println("Input: $n5")
-        println("Output: ${solution.nearestPalindromic(n5)}")  // Ожидается "797"
+        val result = solution.postorderTraversal(root)
+        println(result)  // Output: [3, 2, 1]
 }
