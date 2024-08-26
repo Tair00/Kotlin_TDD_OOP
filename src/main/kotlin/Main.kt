0f8da40c3a6bab81fb1c2e8bc70638ac299f8896
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-26 10:20:01 +0300

    postorderHelper

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
 fun main() {
-        val root = TreeNode(1)
-        root.right = TreeNode(2)
-        root.right?.left = TreeNode(3)
+        val root = Node(1)
+        val child1 = Node(3)
+        val child2 = Node(2)
+        val child3 = Node(4)
+        child1.children = listOf(Node(5), Node(6))
+        root.children = listOf(child1, child2, child3)
 
         val solution = Solution()
-        val result = solution.postorderTraversal(root)
-        println(result)  // Output: [3, 2, 1]
+
+        val result = solution.postorder(root)
+        println(result)  // Output: [5, 6, 3, 2, 4, 1]
 }
