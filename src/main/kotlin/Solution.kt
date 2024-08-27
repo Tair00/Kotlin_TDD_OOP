01c4a2d0f122202a56763e6b2556c365788a5d7f
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-27 15:13:52 +0300

    maxProbability

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,22 +1,36 @@
-class Node(var `val`: Int) {
-    var children: List<Node?> = listOf()
-}
-
+import java.util.*
 
 class Solution {
-    fun postorder(root: Node?): List<Int> {
-        val result = mutableListOf<Int>()
-        postorderHelper(root, result)
-        return result
-    }
+    fun maxProbability(n: Int, edges: Array<IntArray>, succProb: DoubleArray, start_node: Int, end_node: Int): Double {
 
-    private fun postorderHelper(node: Node?, result: MutableList<Int>) {
-        if (node == null) return
-
-        for (child in node.children) {
-            postorderHelper(child, result)
+        val graph = Array(n) { mutableListOf<Pair<Int, Double>>() }
+        for (i in edges.indices) {
+            val (u, v) = edges[i]
+            val prob = succProb[i]
+            graph[u].add(Pair(v, prob))
+            graph[v].add(Pair(u, prob))
         }
 
-        result.add(node.`val`)
+        val maxProb = DoubleArray(n) { 0.0 }
+        maxProb[start_node] = 1.0
+        val pq = PriorityQueue<Pair<Int, Double>>(compareByDescending { it.second })
+        pq.add(Pair(start_node, 1.0))
+
+
+        while (pq.isNotEmpty()) {
+            val (currentNode, currentProb) = pq.poll()
+
+            if (currentNode == end_node) return currentProb
+
+            for ((neighbor, prob) in graph[currentNode]) {
+                val newProb = currentProb * prob
+                if (newProb > maxProb[neighbor]) {
+                    maxProb[neighbor] = newProb
+                    pq.add(Pair(neighbor, newProb))
+                }
+            }
+        }
+
+        return 0.0
     }
 }
