94c26fb7d7e9ab9fb52684d23bffc58bc09d4a1a
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-28 14:04:36 +0300

    countSubIslands

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,18 +1,37 @@
 class Solution {
-    fun climbStairs(n: Int): Int {
-        if (n == 1) return 1
-        if (n == 2) return 2
+    fun countSubIslands(grid1: Array<IntArray>, grid2: Array<IntArray>): Int {
+        val rows = grid1.size
+        val cols = grid1[0].size
+        var subIslandCount = 0
 
-        var oneStepBefore = 2
-        var twoStepsBefore = 1
-        var allWays = 0
+        fun dfs(r: Int, c: Int): Boolean {
+            if (r < 0 || r >= rows || c < 0 || c >= cols || grid2[r][c] == 0) {
+                return true
+            }
 
-        for (i in 3..n) {
-            allWays = oneStepBefore + twoStepsBefore
-            twoStepsBefore = oneStepBefore
-            oneStepBefore = allWays
+            grid2[r][c] = 0
+
+            var isSubIsland = grid1[r][c] == 1
+
+
+            isSubIsland = dfs(r + 1, c) && isSubIsland
+            isSubIsland = dfs(r - 1, c) && isSubIsland
+            isSubIsland = dfs(r, c + 1) && isSubIsland
+            isSubIsland = dfs(r, c - 1) && isSubIsland
+
+            return isSubIsland
         }
 
-        return allWays
+        for (r in 0 until rows) {
+            for (c in 0 until cols) {
+                if (grid2[r][c] == 1) {
+                    if (dfs(r, c)) {
+                        subIslandCount++
+                    }
+                }
+            }
+        }
+
+        return subIslandCount
     }
 }
