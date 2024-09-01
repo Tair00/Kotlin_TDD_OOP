9c2878754d6699c799b43a4bd30d3dc4cb72fa2c
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-01 13:24:39 +0300

    construct2DArray

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,24 @@
-import java.util.*
-
 fun main() {
         val solution = Solution()
 
-        // Test Case 1
-        val n1 = 3
-        val edges1 = arrayOf(
-                intArrayOf(0, 1),
-                intArrayOf(1, 2),
-                intArrayOf(0, 2)
-        )
-        val succProb1 = doubleArrayOf(0.5, 0.5, 0.2)
-        val startNode1 = 0
-        val endNode1 = 2
-        val result1 = solution.maxProbability(n1, edges1, succProb1, startNode1, endNode1)
-        println("Max Probability for Test Case 1: $result1")  // Expected: 0.25
+        // Example 1
+        val original1 = intArrayOf(1, 2, 3, 4)
+        val m1 = 2
+        val n1 = 2
+        val result1 = solution.construct2DArray(original1, m1, n1)
+        println(result1.contentDeepToString())  // Output: [[1, 2], [3, 4]]
 
-        // Test Case 2
+        // Example 2
+        val original2 = intArrayOf(1, 2, 3)
+        val m2 = 1
         val n2 = 3
-        val edges2 = arrayOf(
-                intArrayOf(0, 1),
-                intArrayOf(1, 2),
-                intArrayOf(0, 2)
-        )
-        val succProb2 = doubleArrayOf(0.5, 0.5, 0.3)
-        val startNode2 = 0
-        val endNode2 = 2
-        val result2 = solution.maxProbability(n2, edges2, succProb2, startNode2, endNode2)
-        println("Max Probability for Test Case 2: $result2")  // Expected: 0.3
+        val result2 = solution.construct2DArray(original2, m2, n2)
+        println(result2.contentDeepToString())  // Output: [[1, 2, 3]]
 
-        // Test Case 3
-        val n3 = 3
-        val edges3 = arrayOf(
-                intArrayOf(0, 1),
-                intArrayOf(1, 2)
-        )
-        val succProb3 = doubleArrayOf(0.5, 0.5)
-        val startNode3 = 0
-        val endNode3 = 2
-        val result3 = solution.maxProbability(n3, edges3, succProb3, startNode3, endNode3)
-        println("Max Probability for Test Case 3: $result3")  // Expected: 0.25
-
-        // Test Case 4
-        val n4 = 2
-        val edges4 = arrayOf(
-                intArrayOf(0, 1)
-        )
-        val succProb4 = doubleArrayOf(0.5)
-        val startNode4 = 0
-        val endNode4 = 1
-        val result4 = solution.maxProbability(n4, edges4, succProb4, startNode4, endNode4)
-        println("Max Probability for Test Case 4: $result4")  // Expected: 0.5
+        // Example 3
+        val original3 = intArrayOf(1, 2)
+        val m3 = 1
+        val n3 = 1
+        val result3 = solution.construct2DArray(original3, m3, n3)
+        println(result3.contentDeepToString())  // Output: []
 }
