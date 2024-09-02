50922c5af9508492aa6d0de210f513cb47da9d01
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-02 08:02:38 +0300

    chalkReplacer

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,20 +1,19 @@
 class Solution {
-    fun construct2DArray(original: IntArray, m: Int, n: Int): Array<IntArray> {
-        // If the total number of elements in the original array is not equal to m * n, return an empty array
-        if (original.size != m * n) {
-            return emptyArray()
+    fun chalkReplacer(chalk: IntArray, k: Int): Int {
+
+        val totalChalk = chalk.map { it.toLong() }.sum()
+
+
+        var remainingChalk = k % totalChalk
+
+
+        for (i in chalk.indices) {
+            if (remainingChalk < chalk[i]) {
+                return i
+            }
+            remainingChalk -= chalk[i]
         }
 
-        // Initialize a 2D array with m rows and n columns
-        val result = Array(m) { IntArray(n) }
-
-        // Fill the 2D array row by row
-        for (i in original.indices) {
-            val row = i / n
-            val col = i % n
-            result[row][col] = original[i]
-        }
-
-        return result
+        return -1
     }
 }
