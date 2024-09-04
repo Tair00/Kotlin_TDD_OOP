b3aa345dacd7515dd6fe8e1b5ff6c869d8b02d4f
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-04 15:06:21 +0300

    robotSim

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,9 @@
 fun main() {
+        val commands = intArrayOf(4, -1, 4, -2, 4)
+        val obstacles = arrayOf(intArrayOf(2, 4))
+
         val solution = Solution()
+        val result = solution.robotSim(commands, obstacles)
 
-        // Пример 1:
-        val s1 = "iiii"
-        val k1 = 1
-        val result1 = solution.getLucky(s1, k1)
-        println("Результат для s = \"$s1\" и k = $k1: $result1") // Ожидаемый результат: 36
-
-        // Пример 2:
-        val s2 = "leetcode"
-        val k2 = 2
-        val result2 = solution.getLucky(s2, k2)
-        println("Результат для s = \"$s2\" и k = $k2: $result2") // Ожидаемый результат: 6
-
-        // Пример 3:
-        val s3 = "zbax"
-        val k3 = 2
-        val result3 = solution.getLucky(s3, k3)
-        println("Результат для s = \"$s3\" и k = $k3: $result3") // Ожидаемый результат: 8
+        println("Maximum Euclidean distance squared: $result")
 }
