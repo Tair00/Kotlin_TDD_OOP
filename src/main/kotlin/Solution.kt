6f37437d3bbf018c96cccf28f83c801278074392
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-05 08:01:24 +0300

    missingRolls

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,52 +1,28 @@
 class Solution {
-    fun robotSim(commands: IntArray, obstacles: Array<IntArray>): Int {
+    fun missingRolls(rolls: IntArray, mean: Int, n: Int): IntArray {
 
-        val directions = arrayOf(
-            intArrayOf(0, 1),  // north
-            intArrayOf(1, 0),  // east
-            intArrayOf(0, -1), // south
-            intArrayOf(-1, 0)  // west
-        )
-
-        var x = 0
-        var y = 0
-        var direction = 0
+        val m = rolls.size
+        val totalSum = mean * (n + m)
 
 
-        val obstacleSet = obstacles.map { it[0] to it[1] }.toSet()
+        val knownSum = rolls.sum()
 
 
-        var maxDistanceSquared = 0
+        val missingSum = totalSum - knownSum
 
-        for (command in commands) {
-            when (command) {
-                -2 -> {
-                    // Turn left
-                    direction = (direction + 3) % 4
-                }
-                -1 -> {
 
-                    direction = (direction + 1) % 4
-                }
-                else -> {
-
-                    for (step in 1..command) {
-                        val nextX = x + directions[direction][0]
-                        val nextY = y + directions[direction][1]
-                        if (obstacleSet.contains(nextX to nextY)) {
-                            break
-                        }
-                        x = nextX
-                        y = nextY
-                    }
-                    val distanceSquared = x * x + y * y
-                    if (distanceSquared > maxDistanceSquared) {
-                        maxDistanceSquared = distanceSquared
-                    }
-                }
-            }
+        if (missingSum < n || missingSum > 6 * n) {
+            return intArrayOf() // No valid solution
         }
 
-        return maxDistanceSquared
+
+        val result = IntArray(n) { missingSum / n }
+        var remainder = missingSum % n
+        
+        for (i in 0 until remainder) {
+            result[i]++
+        }
+
+        return result
     }
 }
