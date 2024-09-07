20705f83f8eddb64138789d758d020b186edf266
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-07 11:00:48 +0300

    checkSubPath

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,16 @@
 fun main() {
+    val solution = Solution()
+    val head = ListNode(4)
+    head.next = ListNode(2)
+    head.next?.next = ListNode(8)
+    val root = TreeNode(1)
+    root.left = TreeNode(4)
+    root.right = TreeNode(4)
+    root.left?.right = TreeNode(2)
+    root.right?.right = TreeNode(2)
+    root.left?.right?.left = TreeNode(6)
+    root.left?.right?.right = TreeNode(8)
 
-        val nums1 = intArrayOf(1, 2, 3)
-        val head1 = createLinkedList(intArrayOf(1, 2, 3, 4, 5))
-        val solution = Solution()
-        val result1 = solution.modifiedList(nums1, head1)
-        print("Example 1 result: ")
-        printLinkedList(result1)  // Output: 4 5
-
-        val nums2 = intArrayOf(1)
-        val head2 = createLinkedList(intArrayOf(1, 2, 1, 2, 1, 2))
-        val result2 = solution.modifiedList(nums2, head2)
-        print("Example 2 result: ")
-        printLinkedList(result2)  // Output: 2 2 2
-
-        val nums3 = intArrayOf(5)
-        val head3 = createLinkedList(intArrayOf(1, 2, 3, 4))
-        val result3 = solution.modifiedList(nums3, head3)
-        print("Example 3 result: ")
-        printLinkedList(result3)  // Output: 1 2 3 4
+    val result = solution.isSubPath(head, root)
+    println(result)
 }
