470c655331bee1795f9b9d1698d0929d946c1bd7
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-08 09:37:33 +0300

    splitListToParts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,43 @@
 fun main() {
-    val solution = Solution()
-    val head = ListNode(4)
-    head.next = ListNode(2)
-    head.next?.next = ListNode(8)
-    val root = TreeNode(1)
-    root.left = TreeNode(4)
-    root.right = TreeNode(4)
-    root.left?.right = TreeNode(2)
-    root.right?.right = TreeNode(2)
-    root.left?.right?.left = TreeNode(6)
-    root.left?.right?.right = TreeNode(8)
 
-    val result = solution.isSubPath(head, root)
-    println(result)
+    fun createLinkedList(arr: IntArray): ListNode? {
+        if (arr.isEmpty()) return null
+        val head = ListNode(arr[0])
+        var current = head
+        for (i in 1 until arr.size) {
+            current.next = ListNode(arr[i])
+            current = current.next!!
+        }
+        return head
+    }
+
+
+    fun printLinkedList(head: ListNode?) {
+        var current = head
+        while (current != null) {
+            print("${current!!.`val`} ")
+            current = current!!.next
+        }
+        println()
+    }
+
+
+    val head1 = createLinkedList(intArrayOf(1, 2, 3))
+    val k1 = 5
+    val solution = Solution()
+    val result1 = solution.splitListToParts(head1, k1)
+
+    println("Example 1 result:")
+    for (part in result1) {
+        printLinkedList(part)
+    }
+
+    val head2 = createLinkedList(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    val k2 = 3
+    val result2 = solution.splitListToParts(head2, k2)
+
+    println("Example 2 result:")
+    for (part in result2) {
+        printLinkedList(part)
+    }
 }
