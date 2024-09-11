f4ed96216bad04eb4d656f5c0fcdf9a7f41f57e4
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-11 11:23:56 +0300

    minBitFlips

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,14 @@
 fun main() {
 
-    val head = ListNode(18)
-    head.next = ListNode(6)
-    head.next?.next = ListNode(10)
-    head.next?.next?.next = ListNode(3)
-
-    println("Original list:")
-    printLinkedList(head)
-
     val solution = Solution()
-    val modifiedHead = solution.insertGreatestCommonDivisors(head)
 
-    println("Modified list with GCD nodes inserted:")
-    printLinkedList(modifiedHead)
-}
 
-fun printLinkedList(head: ListNode?) {
-    var current = head
-    while (current != null) {
-        print("${current.`val`} ")
-        current = current.next
-    }
-    println()
+    val start1 = 10
+    val goal1 = 7
+    println("Minimum bit flips to convert $start1 to $goal1: ${solution.minBitFlips(start1, goal1)}")
+
+
+    val start2 = 3
+    val goal2 = 4
+    println("Minimum bit flips to convert $start2 to $goal2: ${solution.minBitFlips(start2, goal2)}")
 }
