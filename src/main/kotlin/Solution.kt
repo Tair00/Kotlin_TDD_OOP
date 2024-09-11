2b7db09086357d415f65e449723386025e140069
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-11 11:23:30 +0300

    minBitFlips

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,32 +1,15 @@
-import kotlin.math.*
-
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-}
-
 class Solution {
-    fun insertGreatestCommonDivisors(head: ListNode?): ListNode? {
-        var current = head
-
-        while (current != null && current.next != null) {e
-            val gcdValue = gcd(current.`val`, current.next!!.`val`)
-
-
-            val newNode = ListNode(gcdValue)
-
-
-            newNode.next = current.next
-            current.next = newNode
-
-
-            current = newNode.next
+    fun minBitFlips(start: Int, goal: Int): Int {
+        val c = start.xor(goal)
+        var pp = 1
+        var r = 0
+        while (pp <= c) {
+            var d = pp.and(c)
+            if (d != 0) {
+                r++
+            }
+            pp *= 2
         }
-
-        return head
-    }
-
-    private fun gcd(a: Int, b: Int): Int {
-        if (b == 0) return a
-        return gcd(b, a % b)
+        return r
     }
 }
