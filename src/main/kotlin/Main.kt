10cf6e9d279f7536f74df26b5ece728425527435
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-12 11:32:44 +0300

    countConsistentStrings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,10 @@ fun main() {
 
     val solution = Solution()
 
+    val allowed = "abc"
 
-    val start1 = 10
-    val goal1 = 7
-    println("Minimum bit flips to convert $start1 to $goal1: ${solution.minBitFlips(start1, goal1)}")
+    val words = arrayOf("a", "b", "c", "ab", "ac", "bc", "abc", "d", "abcd")
 
-
-    val start2 = 3
-    val goal2 = 4
-    println("Minimum bit flips to convert $start2 to $goal2: ${solution.minBitFlips(start2, goal2)}")
+    val result = solution.countConsistentStrings(allowed, words)
+    println("Количество согласованных строк: $result")
 }
