9deb79f0a9010e4cfbb8db725611dbda42982cc1
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-14 09:43:49 +0300

    longestSubarray

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-
     val solution = Solution()
 
-    val allowed = "abc"
+    val nums1 = intArrayOf(1, 2, 3, 3, 2, 2)
+    val result1 = solution.longestSubarray(nums1)
+    println("Result for nums1: $result1") // Ожидаемый результат: 2
 
-    val words = arrayOf("a", "b", "c", "ab", "ac", "bc", "abc", "d", "abcd")
-
-    val result = solution.countConsistentStrings(allowed, words)
-    println("Количество согласованных строк: $result")
+    val nums2 = intArrayOf(1, 2, 3, 4)
+    val result2 = solution.longestSubarray(nums2)
+    println("Result for nums2: $result2") // Ожидаемый результат: 1
 }
