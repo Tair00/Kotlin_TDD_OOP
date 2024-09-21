58f84928356fc618fbf92d82cb5f051c690d4262
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-21 17:45:12 +0300

    lexicalOrder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,6 @@
 fun main() {
     val solution = Solution()
-
-    // Пример 1
-    val timePoints1 = listOf("23:59", "00:00")
-    val result1 = solution.findMinDifference(timePoints1)
-    println("Минимальная разница для timePoints1: $result1")  // Ожидаемый результат: 1
-
-    // Пример 2
-    val timePoints2 = listOf("00:00", "23:59", "00:00")
-    val result2 = solution.findMinDifference(timePoints2)
-    println("Минимальная разница для timePoints2: $result2")  // Ожидаемый результат: 0
+    val n = 13  // Example input
+    val result = solution.lexicalOrder(n)
+    println(result)  // Output: [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
 }
