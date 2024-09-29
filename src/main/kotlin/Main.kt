d3a00244e0d0b5bd0fae0856d0a55f69d304ce6c
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-29 14:15:55 +0300

    AllOne

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,33 @@
 fun main() {
-    val myCircularDeque = MyCircularDeque(3)
-    println(myCircularDeque.insertFront(9)) // returns true
-    println(myCircularDeque.getRear())      // returns 9
-    println(myCircularDeque.insertFront(9)) // returns true
-    println(myCircularDeque.getRear())      // returns 9
-    println(myCircularDeque.insertLast(5))  // returns true
-    println(myCircularDeque.getFront())     // returns 9
-    println(myCircularDeque.getRear())      // returns 5
-    println(myCircularDeque.insertLast(8))  // returns false (deque is full)
-    println(myCircularDeque.deleteLast())   // returns true
-    println(myCircularDeque.getFront())     // returns 9
+    val allOne = AllOne() // Создаем объект AllOne
+
+    // Пример ввода данных и вызова методов
+
+    // Увеличиваем количество для ключей "hello", "goodbye", "hello", "hello"
+    allOne.inc("hello")
+    allOne.inc("goodbye")
+    allOne.inc("hello")
+    allOne.inc("hello")
+
+    // Выводим ключ с максимальным количеством
+    println("Max Key: " + allOne.getMaxKey()) // Ожидается "hello"
+
+    // Увеличиваем количество для ключей "leet", "code", "leet"
+    allOne.inc("leet")
+    allOne.inc("code")
+    allOne.inc("leet")
+
+    // Уменьшаем количество для ключа "hello"
+    allOne.dec("hello")
+
+    // Увеличиваем количество для ключей "leet", "code", "code"
+    allOne.inc("leet")
+    allOne.inc("code")
+    allOne.inc("code")
+
+    // Выводим ключ с максимальным количеством после изменений
+    println("Max Key: " + allOne.getMaxKey()) // Ожидается "leet"
+
+    // Выводим ключ с минимальным количеством
+    println("Min Key: " + allOne.getMinKey()) // Ожидается "goodbye"
 }
