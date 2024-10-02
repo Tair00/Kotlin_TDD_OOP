e233759f5347cccd864dfa7dfd638da01cd7b740
Tair00 <suleimanov.t.i.20@gmail.com>
2024-10-02 19:31:32 +0300

    arrayRankTransform

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,22 +1,12 @@
 class Solution {
-    fun lexicalOrder(n: Int): List<Int> {
-        val result = mutableListOf<Int>()
-        var current = 1
+    fun arrayRankTransform(arr: IntArray): IntArray {
 
-        for (i in 1..n) {
-            result.add(current)
-            if (current * 10 <= n) {
-                current *= 10
-            } else {
-                if (current >= n) {
-                    current /= 10
-                }
-                current += 1
-                while (current % 10 == 0) {
-                    current /= 10
-                }
-            }
+        val sortedUnique = arr.distinct().sorted()
+
+        val rankMap = mutableMapOf<Int, Int>()
+        for (i in sortedUnique.indices) {
+            rankMap[sortedUnique[i]] = i + 1
         }
-        return result
+        return arr.map { rankMap[it]!! }.toIntArray()
     }
 }
