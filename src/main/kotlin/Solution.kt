777593b34d1aeabd712116c7ecc999666e4a8de8
Tair00 <suleimanov.t.i.20@gmail.com>
2024-10-05 14:41:04 +0300

    checkInclusion

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,12 +1,28 @@
 class Solution {
-    fun arrayRankTransform(arr: IntArray): IntArray {
+    fun checkInclusion(s1: String, s2: String): Boolean {
+        val len1 = s1.length
+        val len2 = s2.length
 
-        val sortedUnique = arr.distinct().sorted()
+        if (len1 > len2) return false
 
-        val rankMap = mutableMapOf<Int, Int>()
-        for (i in sortedUnique.indices) {
-            rankMap[sortedUnique[i]] = i + 1
+        val count1 = IntArray(26)
+        val count2 = IntArray(26)
+        for (i in s1.indices) {
+            count1[s1[i] - 'a']++
+            count2[s2[i] - 'a']++
         }
-        return arr.map { rankMap[it]!! }.toIntArray()
+
+        if (count1 contentEquals count2) return true
+
+        for (i in len1 until len2) {
+
+            count2[s2[i] - 'a']++
+
+            count2[s2[i - len1] - 'a']--
+
+            if (count1 contentEquals count2) return true
+        }
+
+        return false
     }
 }
