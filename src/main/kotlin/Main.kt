ccb5ad7b1b199a4dc86131337f19c14a267f08a7
Tair00 <suleimanov.t.i.20@gmail.com>
2024-10-05 14:41:23 +0300

    checkInclusion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-
-    val arr1 = intArrayOf(40, 10, 20, 30)
-    val arr2 = intArrayOf(100, 100, 100)
-    val arr3 = intArrayOf(37, 12, 28, 9, 100, 56, 80, 5, 12)
+    val s1 = "ab"
+    val s2 = "eidbaooo"
 
     val solution = Solution()
-    println(solution.arrayRankTransform(arr1).joinToString(", ")) // Expected Output: [4, 1, 2, 3]
-    println(solution.arrayRankTransform(arr2).joinToString(", ")) // Expected Output: [1, 1, 1]
-    println(solution.arrayRankTransform(arr3).joinToString(", ")) // Expected Output: [5, 3, 4, 2, 8, 6, 7, 1, 3]
+
+    val result = solution.checkInclusion(s1, s2)
+
+    // Печатаем результат
+    println("Does s2 contain a permutation of s1? $result")
 }
