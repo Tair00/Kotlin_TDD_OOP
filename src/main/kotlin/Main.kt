4f8236471ced234fd06b34852eb9bb2f5d92d027
Tair00 <suleimanov.t.i.20@gmail.com>
2024-10-22 20:32:52 +0300

    kthLargestLevelSum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,20 @@
 fun main() {
-    val walk: MoveLogic = Walk()
-    val fly: MoveLogic = Fly()
 
-    val animalA: Animal = Person(walk)
-    val animalB: Animal = Bird(fly)
+    val root = TreeNode(154282)
+    root.left = TreeNode(259410)
+    root.right = TreeNode(741548)
 
-    animalA.howDoIMove()
-    animalB.howDoIMove()
+    root.left?.left = TreeNode(66685)
+    root.left?.right = TreeNode(834946)
+    root.right?.left = TreeNode(24416)
+    root.right?.right = TreeNode(49355)
+
+
+    val solution = Solution()
+
+
+    val k = 3
+    val result = solution.kthLargestLevelSum(root, k)
+
+    println("Результат: $result")
 }
