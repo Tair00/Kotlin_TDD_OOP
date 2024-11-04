94b55bd8a8a8632a82f0a30297eb573d5bc8cef6
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-04 12:13:05 +0300

    compressedString

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,7 +1,19 @@
 class Solution {
-    fun rotateString(s: String, goal: String): Boolean {
-        if (s.length != goal.length) return false
-        return (s + s).contains(goal)
-    }
+    fun compressedString(word: String): String {
+        val comp = StringBuilder()
+        var i = 0
 
+        while (i < word.length) {
+                        val c = word[i]
+            var count = 0
+
+                       while (i < word.length && word[i] == c && count < 9) {
+                count++
+                i++
+            }
+            comp.append(count).append(c)
+        }
+
+        return comp.toString()
+    }
 }
