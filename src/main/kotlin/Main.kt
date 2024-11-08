676fe9d6f733a1373182113cc4a91945f2d0c891
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-08 16:47:53 +0300

    getMaximumXor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 fun main() {
+    // Example 1
+    val nums1 = intArrayOf(0, 1, 1, 3)
+    val maximumBit1 = 2
     val solution = Solution()
+    println("Example 1 Output: " + solution.getMaximumXor(nums1, maximumBit1).contentToString())
 
-    val nums1 = intArrayOf(136, 256, 10)
-    println("Can nums1 be sorted? ${solution.canSortArray(nums1)}")  // Expected output: false
+    // Example 2
+    val nums2 = intArrayOf(2, 3, 4, 7)
+    val maximumBit2 = 3
+    println("Example 2 Output: " + solution.getMaximumXor(nums2, maximumBit2).contentToString())
 
-    val nums2 = intArrayOf(8, 4, 2, 30, 15)
-    println("Can nums2 be sorted? ${solution.canSortArray(nums2)}")  // Expected output: true
+    // Example 3
+    val nums3 = intArrayOf(0, 1, 2, 2, 5, 7)
+    val maximumBit3 = 3
+    println("Example 3 Output: " + solution.getMaximumXor(nums3, maximumBit3).contentToString())
 }
