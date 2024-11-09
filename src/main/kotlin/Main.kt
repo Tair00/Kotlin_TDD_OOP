da7ccd37138506e7c050a5860939657e45e7589a
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-09 23:20:16 +0300

    minEnd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,7 @@
 fun main() {
-    // Example 1
-    val nums1 = intArrayOf(0, 1, 1, 3)
-    val maximumBit1 = 2
     val solution = Solution()
-    println("Example 1 Output: " + solution.getMaximumXor(nums1, maximumBit1).contentToString())
-
-    // Example 2
-    val nums2 = intArrayOf(2, 3, 4, 7)
-    val maximumBit2 = 3
-    println("Example 2 Output: " + solution.getMaximumXor(nums2, maximumBit2).contentToString())
-
-    // Example 3
-    val nums3 = intArrayOf(0, 1, 2, 2, 5, 7)
-    val maximumBit3 = 3
-    println("Example 3 Output: " + solution.getMaximumXor(nums3, maximumBit3).contentToString())
+    val n = 3
+    val x = 4
+    val result = solution.minEnd(n, x)
+    println("Minimum possible value of nums[n - 1]: $result")
 }
