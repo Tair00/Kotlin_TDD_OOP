030a6bb4e6ecc831cec8db2b12baa5194bf562e9
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-14 11:56:03 +0300

    minimizedMaximum

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,27 +1,28 @@
 class Solution {
-    fun maximumBeauty(items: Array<IntArray>, queries: IntArray): IntArray {
+    fun minimizedMaximum(n: Int, quantities: IntArray): Int {
+        var left = 1
+        var right = quantities.maxOrNull() ?: 1
 
-        items.sortWith(compareBy({ it[0] }, { it[1] }))
-
-        val maxBeautyUpToPrice = mutableListOf<Pair<Int, Int>>()
-        var currentMaxBeauty = 0
-        for (item in items) {
-            val price = item[0]
-            val beauty = item[1]
-            currentMaxBeauty = maxOf(currentMaxBeauty, beauty)
-            maxBeautyUpToPrice.add(Pair(price, currentMaxBeauty))
-        }
-
-        val sortedQueries = queries.withIndex().sortedBy { it.value }
-        val result = IntArray(queries.size)
-        var i = 0
-        for ((index, query) in sortedQueries) {
-            while (i < maxBeautyUpToPrice.size && maxBeautyUpToPrice[i].first <= query) {
-                i++
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            if (canDistribute(n, quantities, mid)) {
+                right = mid
+            } else {
+                left = mid + 1
             }
-            result[index] = if (i > 0) maxBeautyUpToPrice[i - 1].second else 0
         }
 
-        return result
+        return left
+    }
+
+    private fun canDistribute(n: Int, quantities: IntArray, maxProductsPerStore: Int): Boolean {
+        var storesNeeded = 0
+        for (quantity in quantities) {
+            storesNeeded += (quantity + maxProductsPerStore - 1) / maxProductsPerStore
+            if (storesNeeded > n) {
+                return false
+            }
+        }
+        return true
     }
 }
