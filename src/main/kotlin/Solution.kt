0f8a9845cbe576b25c127b51e15ff99af2a7a58d
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-18 21:15:35 +0300

    decrypt

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,32 +1,18 @@
-import java.util.Deque
-import java.util.LinkedList
-
 class Solution {
-    fun shortestSubarray(nums: IntArray, k: Int): Int {
-        val n = nums.size
-        val prefixSum = LongArray(n + 1)
+    fun decrypt(code: IntArray, k: Int): IntArray {
+        val n = code.size
+        if (k == 0) return IntArray(n) { 0 }
 
-        for (i in nums.indices) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i]
+        val result = IntArray(n)
+        val extended = code + code
+        for (i in code.indices) {
+            if (k > 0) {
+                result[i] = (1..k).sumOf { extended[i + it] }
+            } else {
+                result[i] = (-1 downTo k).sumOf { extended[i + n + it] }
+            }
         }
 
-        var minLength = Int.MAX_VALUE
-        val deque: Deque<Int> = LinkedList()
-
-        for (i in prefixSum.indices) {
-
-            while (deque.isNotEmpty() && prefixSum[i] - prefixSum[deque.first()] >= k) {
-                minLength = minOf(minLength, i - deque.removeFirst())
-            }
-
-
-            while (deque.isNotEmpty() && prefixSum[i] <= prefixSum[deque.last()]) {
-                deque.removeLast()
-            }
-
-            deque.addLast(i)
-        }
-
-        return if (minLength == Int.MAX_VALUE) -1 else minLength
+        return result
     }
 }
