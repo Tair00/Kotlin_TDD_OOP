a68d5f57c37ec3593addedde78b87037d075d57c
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-19 13:51:09 +0300

    maximumSubarraySum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,6 @@
 fun main() {
     val solution = Solution()
-
-    // Example 1
-    val code1 = intArrayOf(5, 7, 1, 4)
+    val nums1 = intArrayOf(1, 5, 4, 2, 9, 9, 9)
     val k1 = 3
-    println("Decrypted code for Example 1: ${solution.decrypt(code1, k1).toList()}")
-
-    // Example 2
-    val code2 = intArrayOf(1, 2, 3, 4)
-    val k2 = 0
-    println("Decrypted code for Example 2: ${solution.decrypt(code2, k2).toList()}")
-
-    // Example 3
-    val code3 = intArrayOf(2, 4, 9, 3)
-    val k3 = -2
-    println("Decrypted code for Example 3: ${solution.decrypt(code3, k3).toList()}")
+    println(solution.maximumSubarraySum(nums1, k1))
 }
