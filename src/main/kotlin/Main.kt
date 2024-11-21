ec5c7dc4ce326b056ee2d7b0ad30d072b915ac54
Tair00 <suleimanov.t.i.20@gmail.com>
2024-11-21 18:27:22 +0300

    diffieHellmanKeyExchange

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,59 @@
+import java.io.File
+import kotlin.math.pow
+import kotlin.random.Random
+
+fun generateRandomNumber(limit: Int): Int {
+    return Random.nextInt(2, limit - 1)
+}
+
+fun computeModularExponentiation(base: Int, exponent: Int, modulus: Int): Int {
+    return base.toDouble().pow(exponent).toInt() % modulus
+}
+
+fun diffieHellmanKeyExchange() {
+    // Инициализация известных параметров
+    val base = Random.nextInt(2, 20) // Основание
+    val modulus = Random.nextInt(100, 200) // Модуль (простое число)
+
+    // Секретные числа
+    val privateKeyA = generateRandomNumber(modulus)
+    val privateKeyB = generateRandomNumber(modulus)
+
+    // Открытые значения
+    val publicValueA = computeModularExponentiation(base, privateKeyA, modulus)
+    val publicValueB = computeModularExponentiation(base, privateKeyB, modulus)
+
+    // Вычисление общего секретного ключа
+    val sharedSecretA = computeModularExponentiation(publicValueB, privateKeyA, modulus)
+    val sharedSecretB = computeModularExponentiation(publicValueA, privateKeyB, modulus)
+
+    // Сохранение данных в файлы
+    File("public_keys.txt").writeText(
+        "Base (a): $base\nModulus (m): $modulus\n" +
+                "Public Value A (L0): $publicValueA\nPublic Value B (Lp): $publicValueB\n"
+    )
+
+    File("private_keys.txt").writeText(
+        "Private Key A: $privateKeyA\nPrivate Key B: $privateKeyB\n"
+    )
+
+    File("shared_secret.txt").writeText(
+        "Shared Secret Key (calculated by A): $sharedSecretA\n" +
+                "Shared Secret Key (calculated by B): $sharedSecretB\n"
+    )
+
+    // Проверка
+    if (sharedSecretA == sharedSecretB) {
+        println("Общий секретный ключ успешно создан: $sharedSecretA")
+    } else {
+        println("Ошибка: общий ключи не совпадают!")
+    }
+}
+
 fun main() {
-    val solution = Solution()
-    val nums1 = intArrayOf(1, 5, 4, 2, 9, 9, 9)
-    val k1 = 3
-    println(solution.maximumSubarraySum(nums1, k1))
+    // Тестирование с 10 наборами данных
+    for (i in 1..10) {
+        println("\nТест $i:")
+        diffieHellmanKeyExchange()
+    }
 }
